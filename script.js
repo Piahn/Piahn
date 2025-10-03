877e62c2aa3a40ed850ea177911b302e51deafb6
PIahn Wijaya <132025108+Piahn@users.noreply.github.com>
2025-10-03 19:39:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
 
+
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
 });
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+
 });
